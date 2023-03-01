6d0546d2735dc476edfc7f9e1a8e51f04ad3b37a
skthati <97585808+skthati@users.noreply.github.com>
2023-03-01 15:40:10 +1300

    Add files via upload
    1) Using Python function to add two numbers.

---
+++ Add two numbers.py
@@ -0,0 +1,12 @@
+
+# Add two numbers
+
+a = int(input("Enter your first Number"))
+b = int(input("Enter your Second Number"))
+
+def add_numbers(a,b):
+    return a + b;
+
+print(str(a) + " + " + str(b) + " = " + str(add_numbers(a,b)))  
+
+
